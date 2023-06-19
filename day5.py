f2a914d7a549d34ef4ee6bf0a0ad4091fd2bf565
SauBanh <saubanh05062001@gmail.com>
2023-06-19 15:04:15 +0700

    update

--- day5.py
+++ day5.py
@@ -48,8 +48,9 @@
 #     total += number
 # print(total)
 
-total = 0
-for number in range(1,101):
-    if number % 2 == 0:
-        total += number
-print(total)
+# total = 0
+# for number in range(1,101):
+#     if number % 2 == 0:
+#         total += number
+# print(total)
+
